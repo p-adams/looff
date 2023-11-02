e49abde5bd9598b7987c58a82f93e227ae9779dc
Paul Adams <ghusiyy@gmail.com>
2023-11-02 00:21:46 -0700

    just use swiper

--- src/Carousel.tsx
+++ src/Carousel.tsx
@@ -1,25 +1,29 @@
-import "./Carousel.css";
-export default function Carousel() {
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
+import "swiper/css";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+
+export default () => {
   return (
-    <div className="Carousel--outer">
-      <div className="item">
-        <h2>A</h2>
-      </div>
-      <div className="item">
-        <h2>B</h2>
-      </div>
-      <div className="item">
-        <h2>C</h2>
-      </div>
-      <div className="item">
-        <h2>D</h2>
-      </div>
-      <div className="item">
-        <h2>E</h2>
-      </div>
-      <div className="item">
-        <h2>F</h2>
-      </div>
-    </div>
+    <Swiper
+      modules={[Navigation, Pagination, Scrollbar, A11y]}
+      navigation={true}
+      spaceBetween={10}
+      slidesPerView={5}
+      onSlideChange={() => console.log("slide change")}
+      onSwiper={(swiper) => console.log(swiper)}
+    >
+      <SwiperSlide>Slide 1</SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
+      <SwiperSlide>Slide 5</SwiperSlide>
+      <SwiperSlide>Slide 6</SwiperSlide>
+      <SwiperSlide>Slide 7</SwiperSlide>
+      <SwiperSlide>Slide 8</SwiperSlide>
+    </Swiper>
   );
-}
+};
