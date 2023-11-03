b2d9635c6c85439250b1d5c3484ece17d7b5628b
Paul Adams <ghusiyy@gmail.com>
2023-11-03 00:44:40 -0700

    add card demo

--- src/App.tsx
+++ src/App.tsx
@@ -6,40 +6,46 @@ import Thumbnail from "./Thumbnail";
 import { useState } from "react";
 
 function App() {
-  const [demoModes, setDemoMode] = useState("thumbnail");
+  const [demoMode, setDemoMode] = useState("thumbnail");
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-      </div>
+      <div></div>
       <h1>Looff Carousel Component</h1>
       <div className="card">
         <label htmlFor="demoMode">
-          Demo Modes
+          Demo Modes:
           <select id="demoMode" onChange={(e) => setDemoMode(e.target.value)}>
             <option value={"thumbnail"}>Thumbnail</option>
             <option value={"thumbnail_responsive"}>Responsive Thumbnail</option>
+            <option value={"card"}>Card</option>
+            <option value={"card_responsive"}>Responsive Card</option>
           </select>
         </label>
-        {demoModes === "thumbnail" && (
+        {demoMode === "thumbnail" && (
           <section>
             <h2>Thumbnails</h2>
             <Carousel renderSlideContent={() => <Thumbnail />} />
           </section>
         )}
-        {demoModes === "thumbnail_responsive" && (
+        {demoMode === "thumbnail_responsive" && (
           <section>
             <h2>Responsive Thumbnails</h2>
             <Carousel renderSlideContent={() => <Thumbnail />} />
           </section>
         )}
+        {demoMode === "card" && (
+          <section>
+            <h2>Card</h2>
+            <Carousel renderSlideContent={() => <Card />} />
+          </section>
+        )}
+        {demoMode === "card_responsive" && (
+          <section>
+            <h2>Responsive Card</h2>
+            <Carousel renderSlideContent={() => <Card />} />
+          </section>
+        )}
       </div>
-
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
