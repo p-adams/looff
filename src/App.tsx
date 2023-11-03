55b17562a24183a30944e9ec14d6288aa8f70b4c
Paul Adams <ghusiyy@gmail.com>
2023-11-03 00:39:44 -0700

    add demo modes

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,10 @@ import "./App.css";
 import Carousel from "./Carousel";
 import Card from "./Card";
 import Thumbnail from "./Thumbnail";
+import { useState } from "react";
 
 function App() {
+  const [demoModes, setDemoMode] = useState("thumbnail");
   return (
     <>
       <div>
@@ -14,14 +16,25 @@ function App() {
       </div>
       <h1>Looff Carousel Component</h1>
       <div className="card">
-        <section>
-          <h2>Thumbnails</h2>
-          <Carousel renderSlideContent={() => <Thumbnail />} />
-        </section>
-        {/* <section>
-          <h2> Responsive Thumbnails</h2>
-          <Carousel renderSlideContent={() => <Thumbnail />} />
-  </section> */}
+        <label htmlFor="demoMode">
+          Demo Modes
+          <select id="demoMode" onChange={(e) => setDemoMode(e.target.value)}>
+            <option value={"thumbnail"}>Thumbnail</option>
+            <option value={"thumbnail_responsive"}>Responsive Thumbnail</option>
+          </select>
+        </label>
+        {demoModes === "thumbnail" && (
+          <section>
+            <h2>Thumbnails</h2>
+            <Carousel renderSlideContent={() => <Thumbnail />} />
+          </section>
+        )}
+        {demoModes === "thumbnail_responsive" && (
+          <section>
+            <h2>Responsive Thumbnails</h2>
+            <Carousel renderSlideContent={() => <Thumbnail />} />
+          </section>
+        )}
       </div>
 
       <p className="read-the-docs">
