269f6bc85a135f6b635e4509c550e696b3ebe775
Paul Adams <ghusiyy@gmail.com>
2023-11-05 00:25:20 -0700

    add sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -8,68 +8,81 @@ import { useState } from "react";
 function App() {
   const [demoMode, setDemoMode] = useState("thumbnail");
   return (
-    <>
-      <div></div>
+    <section>
       <h1>Looff Carousel Component</h1>
-      <div className="card">
-        <label htmlFor="demoMode">
-          Demo Modes:
-          <select id="demoMode" onChange={(e) => setDemoMode(e.target.value)}>
-            <option value={"thumbnail"}>Thumbnail</option>
-            <option value={"thumbnail_responsive"}>Responsive Thumbnail</option>
-            <option value={"card"}>Card</option>
-            <option value={"card_responsive"}>Responsive Card</option>
-          </select>
-        </label>
-        {demoMode === "thumbnail" && (
-          <section>
-            <h2>Thumbnails</h2>
-            <Carousel
-              showSlidesInDialog={true}
-              renderSlideContent={() => <Thumbnail />}
-            />
-          </section>
-        )}
-        {demoMode === "thumbnail_responsive" && (
-          <section>
-            <h2>Responsive Thumbnails</h2>
-            <Carousel
-              breakpoints={{
-                400: {
-                  slidesPerView: 1,
-                  spaceBetween: 10,
-                },
-                640: {
-                  slidesPerView: 2,
-                  spaceBetween: 10,
-                },
-                768: {
-                  slidesPerView: 4,
-                  spaceBetween: 10,
-                },
-                1024: {
-                  slidesPerView: 5,
-                  spaceBetween: 10,
-                },
-              }}
-              renderSlideContent={() => <Thumbnail />}
-            />
-          </section>
-        )}
-        {demoMode === "card" && (
-          <section>
-            <h2>Card</h2>
-            <Carousel renderSlideContent={() => <Card />} />
-          </section>
-        )}
-        {demoMode === "card_responsive" && (
-          <section>
-            <h2>Responsive Card</h2>
-            <Carousel renderSlideContent={() => <Card />} />
-          </section>
-        )}
+      <div className="container">
+        <div className="main-content">
+          <label htmlFor="demoMode">
+            Demo Modes:
+            <select id="demoMode" onChange={(e) => setDemoMode(e.target.value)}>
+              <option value={"thumbnail"}>Thumbnail</option>
+              <option value={"thumbnail_responsive"}>
+                Responsive Thumbnail
+              </option>
+              <option value={"card"}>Card</option>
+              <option value={"card_responsive"}>Responsive Card</option>
+            </select>
+          </label>
+          {demoMode === "thumbnail" && (
+            <section>
+              <h2>Thumbnails</h2>
+              <Carousel
+                showSlidesInDialog={true}
+                renderSlideContent={() => <Thumbnail />}
+              />
+            </section>
+          )}
+          {demoMode === "thumbnail_responsive" && (
+            <section>
+              <h2>Responsive Thumbnails</h2>
+              <Carousel
+                breakpoints={{
+                  400: {
+                    slidesPerView: 1,
+                    spaceBetween: 10,
+                  },
+                  640: {
+                    slidesPerView: 2,
+                    spaceBetween: 10,
+                  },
+                  960: {
+                    slidesPerView: 3, // Adjust this value as needed
+                    spaceBetween: 10,
+                  },
+                  1280: {
+                    slidesPerView: 4, // Adjust this value as needed
+                    spaceBetween: 10,
+                  },
+                }}
+                renderSlideContent={() => <Thumbnail />}
+              />
+            </section>
+          )}
+          {demoMode === "card" && (
+            <section>
+              <h2>Card</h2>
+              <Carousel renderSlideContent={() => <Card />} />
+            </section>
+          )}
+          {demoMode === "card_responsive" && (
+            <section>
+              <h2>Responsive Card</h2>
+              <Carousel renderSlideContent={() => <Card />} />
+            </section>
+          )}
+        </div>
+        <aside>
+          <ul>
+            {Array(8)
+              .fill(0)
+              .map((_, i) => ({ label: `data: ${i}` }))
+              .map((d) => (
+                <li>{d.label}</li>
+              ))}
+          </ul>
+        </aside>
       </div>
-    </>
+    </section>
   );
 }
 
